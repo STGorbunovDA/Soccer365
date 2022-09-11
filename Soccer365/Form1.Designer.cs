e17661a9abd2c63433bf8befb781cea65b5cfbcb
Дмитрий Горбунов <bous07@mail.ru>
2022-09-11 21:49:44 +0300

    Добавьте файлы проекта.

---
+++ Soccer365/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Soccer365
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Soccer365 = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.lbl_Percent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Soccer365
+            // 
+            this.btn_Soccer365.Location = new System.Drawing.Point(273, 359);
+            this.btn_Soccer365.Name = "btn_Soccer365";
+            this.btn_Soccer365.Size = new System.Drawing.Size(111, 23);
+            this.btn_Soccer365.TabIndex = 0;
+            this.btn_Soccer365.Text = "Начать парсить";
+            this.btn_Soccer365.UseVisualStyleBackColor = true;
+            this.btn_Soccer365.Click += new System.EventHandler(this.btn_Soccer365_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl.Location = new System.Drawing.Point(12, 64);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(77, 20);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "Команда";
+            // 
+            // lbl_Percent
+            // 
+            this.lbl_Percent.AutoSize = true;
+            this.lbl_Percent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_Percent.Location = new System.Drawing.Point(95, 64);
+            this.lbl_Percent.Name = "lbl_Percent";
+            this.lbl_Percent.Size = new System.Drawing.Size(126, 20);
+            this.lbl_Percent.TabIndex = 2;
+            this.lbl_Percent.Text = "Процент побед";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(667, 450);
+            this.Controls.Add(this.lbl_Percent);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btn_Soccer365);
+            this.Name = "Form1";
+            this.Text = "Парсер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Soccer365;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lbl_Percent;
+    }
+}
+
---
+++ Soccer365/Form1.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soccer365
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, double> _dict;
+        public Form1()
+        {
+            InitializeComponent();
+            _dict = new Dictionary<string, double>();
+        }
+
+        async void btn_Soccer365_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"https://soccer365.ru/";
+
+            driver.FindElement(By.XPath("(//a[@id='hds_item_link'])[3]")).Click();
+            driver.FindElement(By.XPath("(//span[@class='selectbox-label'])[1]")).Click();
+            driver.FindElement(By.XPath("(//a[@href='javascript:void(0)'])[2]")).Click();
+            await Task.Delay(1000);
+            driver.FindElement(By.XPath("//span[contains(.,'А-лига')]")).Click();
+            driver.FindElement(By.XPath("//span[@class='selectbox-label'][contains(.,'2022/2023')]")).Click();
+            driver.FindElement(By.XPath("//a[contains(.,'2021/2022')]")).Click();
+
+            int game = 1;
+            int defeats = 4;
+            for (int i = 1; i < 13; i++)
+            {
+                var lbl_Team = $"{driver.FindElement(By.XPath($"(//a[contains(@rel,'nofollow')])[{i}]")).GetAttribute("textContent")}\r\n";// Записываем команду
+
+                string playGameString = $"{driver.FindElement(By.XPath($"(//td[@class='ctr'])[{game}]")).GetAttribute("textContent")}\r\n";
+
+                string playDefeatsString = $"{driver.FindElement(By.XPath($"(//td[@class='ctr'])[{defeats}]")).GetAttribute("textContent")}\r\n";
+
+                double lbl_Percent = (100 - (Convert.ToDouble(playDefeatsString) / Convert.ToDouble(playGameString)) * 100);
+
+                _dict[lbl_Team] = lbl_Percent;
+                game += 8;
+                defeats += 8;
+            }
+
+            var result = string.Empty;
+
+            foreach (var pair in _dict)
+            {
+                result += $"{pair.Key}={pair.Value}\r\n";
+            }
+            MessageBox.Show(result);
+
+        }
+
+
+    }
+}
