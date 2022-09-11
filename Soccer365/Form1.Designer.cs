e3979dfcc2719675bb19cf7f3e19cdf1d9cdc7bf
Дмитрий Горбунов <bous07@mail.ru>
2022-09-11 21:49:44 +0300

    Добавьте файлы проекта.

---
+++ Soccer365/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Soccer365
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Soccer365 = new System.Windows.Forms.Button();
+            this.cmB_years_game = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btn_Soccer365
+            // 
+            this.btn_Soccer365.Location = new System.Drawing.Point(141, 92);
+            this.btn_Soccer365.Name = "btn_Soccer365";
+            this.btn_Soccer365.Size = new System.Drawing.Size(201, 84);
+            this.btn_Soccer365.TabIndex = 0;
+            this.btn_Soccer365.Text = "Начать парсить";
+            this.btn_Soccer365.UseVisualStyleBackColor = true;
+            this.btn_Soccer365.Click += new System.EventHandler(this.btn_Soccer365_Click);
+            // 
+            // cmB_years_game
+            // 
+            this.cmB_years_game.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cmB_years_game.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmB_years_game.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cmB_years_game.FormattingEnabled = true;
+            this.cmB_years_game.Items.AddRange(new object[] {
+            "2022/2023",
+            "2021/2022",
+            "2020/2021",
+            "2019/2020",
+            "2018/2019",
+            "2017/2018",
+            "2016/2017",
+            "2015/2016"});
+            this.cmB_years_game.Location = new System.Drawing.Point(141, 65);
+            this.cmB_years_game.Name = "cmB_years_game";
+            this.cmB_years_game.Size = new System.Drawing.Size(201, 21);
+            this.cmB_years_game.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(494, 256);
+            this.Controls.Add(this.cmB_years_game);
+            this.Controls.Add(this.btn_Soccer365);
+            this.Name = "Form1";
+            this.Text = "Парсер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Soccer365;
+        private System.Windows.Forms.ComboBox cmB_years_game;
+    }
+}
+
---
+++ Soccer365/Form1.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soccer365
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, double> _dict;
+        public Form1()
+        {
+            InitializeComponent();
+            _dict = new Dictionary<string, double>();
+        }
+
+        async void btn_Soccer365_Click(object sender, EventArgs e)
+        {
+            if (cmB_years_game.Text != "")
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Url = @"https://soccer365.ru/";
+
+                driver.FindElement(By.XPath("(//a[@id='hds_item_link'])[3]")).Click();
+                driver.FindElement(By.XPath("(//span[@class='selectbox-label'])[1]")).Click();
+                driver.FindElement(By.XPath("(//a[@href='javascript:void(0)'])[2]")).Click();
+                await Task.Delay(1000);
+                driver.FindElement(By.XPath("//span[contains(.,'А-лига')]")).Click();
+                //a[contains(.,'')]
+                driver.FindElement(By.XPath("//span[@class='selectbox-label'][contains(.,'2022/2023')]")).Click();
+                driver.FindElement(By.XPath($"//a[contains(.,'{cmB_years_game.Text}')]")).Click();
+
+                int game = 1;
+                int defeats = 4;
+
+                for (int i = 2; i < 14; i++)
+                {
+                    var lbl_Team = $"{driver.FindElement(By.XPath($"(//a[contains(@rel,'nofollow')])[{i}]")).GetAttribute("textContent")}\r\n";// Записываем команду
+
+                    string playGameString = $"{driver.FindElement(By.XPath($"(//td[@class='ctr'])[{game}]")).GetAttribute("textContent")}\r\n";
+
+                    string playDefeatsString = $"{driver.FindElement(By.XPath($"(//td[@class='ctr'])[{defeats}]")).GetAttribute("textContent")}\r\n";
+
+                    double lbl_Percent = (100 - (Convert.ToDouble(playDefeatsString) / Convert.ToDouble(playGameString)) * 100);
+                    if(lbl_Percent > 80) _dict[lbl_Team] = lbl_Percent;
+                    game += 8;
+                    defeats += 8;
+                }
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.Unicode))
+                    {
+                        string note = string.Empty;
+
+                        note += $"Команда\t\tПроцент побед и ничей";
+
+                        sw.WriteLine(note);
+
+                        foreach (var pair in _dict)
+                        {
+                            sw.Write($"{pair.Value} = {pair.Key}");    
+                        }
+
+                        MessageBox.Show("Файл успешно сохранен");
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmB_years_game.SelectedIndex = 1;
+        }
+    }
+}
